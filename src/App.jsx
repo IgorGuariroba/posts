7da2a4625502eb2e491576fa20e06820f734fd27
igor.guariroba <igorguariroba@movida.com.br>
2022-09-20 14:20:30 -0300

    Passagem de parametros para os componentes

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,10 @@ export function App() {
         author="Igor Guariroba"
         content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum quisquam debitis, doloribus autem, eos, in molestias neque consectetur ex facilis corporis recusandae sed voluptas? Necessitatibus atque ducimus autem error aliquid."
         />
-      <Post />
+      <Post 
+            author="Fulano"
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum quisquam debitis, doloribus autem, eos, in molestias neque consectetur ex facilis corporis recusandae sed voluptas? Necessitatibus atque ducimus autem error aliquid."    
+      />
       <Post />
       <Post />
     </div>
