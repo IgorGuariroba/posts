ce7d8bc2f5ed44f5990ee5d50e102c8e4ba2930f
igor.guariroba <igorguariroba@movida.com.br>
2022-09-20 21:59:27 -0300

    Header finalizado

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,11 @@
 
 import styles  from  './Header.module.css';
+import igniteLogo from '../assets/Ignite-logo.svg';
 
 export function Header(){
   return(
     <header className={styles.header}>
-    <strong >Titulo</strong>
+      <img src={igniteLogo} alt="Logotipo do site" />
     </header>
   );
 }
