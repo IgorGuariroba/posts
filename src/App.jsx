27c0a8ea0bf38331ad533f2426bf6901a710bfa2
igor.guariroba <igorguariroba@movida.com.br>
2022-09-21 20:41:40 -0300

    Finalizado a sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,31 @@
 import { Post } from "./Post"
 import { Header } from "./components/Header"
+import { Sidebar } from "./components/Sidebar";
 
+
+import styles from './App.module.css';
 import './global.css';
 
 export function App() {
   return (
     <div>
-      <Header/>
-      <Post
-        author="Igor Guariroba"
-        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum quisquam debitis, doloribus autem, eos, in molestias neque consectetur ex facilis corporis recusandae sed voluptas? Necessitatibus atque ducimus autem error aliquid."
-        />
-      <Post 
+      <Header />
+
+      <div className={styles.wrapper}>
+          <Sidebar/>
+        <main>
+          <Post
+            author="Igor Guariroba"
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum quisquam debitis, doloribus autem, eos, in molestias neque consectetur ex facilis corporis recusandae sed voluptas? Necessitatibus atque ducimus autem error aliquid."
+          />
+          <Post
             author="Fulano"
-            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum quisquam debitis, doloribus autem, eos, in molestias neque consectetur ex facilis corporis recusandae sed voluptas? Necessitatibus atque ducimus autem error aliquid."    
-      />
-      <Post />
-      <Post />
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum quisquam debitis, doloribus autem, eos, in molestias neque consectetur ex facilis corporis recusandae sed voluptas? Necessitatibus atque ducimus autem error aliquid."
+          />
+          <Post />
+          <Post />
+        </main>
+      </div>
     </div>
   )
 }
