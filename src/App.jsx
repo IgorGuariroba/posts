4002ad0a385847908f2a04b2842c7935bdbcd3f1
igor.guariroba <igorguariroba@movida.com.br>
2022-09-20 21:04:02 -0300

    Entendendo o css modules e como ele funciona no react

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 import { Post } from "./Post"
+import { Header } from "./components/Header"
+
+import './styles.css';
 
 export function App() {
   return (
     <div>
+      <Header/>
       <Post
         author="Igor Guariroba"
         content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum quisquam debitis, doloribus autem, eos, in molestias neque consectetur ex facilis corporis recusandae sed voluptas? Necessitatibus atque ducimus autem error aliquid."
