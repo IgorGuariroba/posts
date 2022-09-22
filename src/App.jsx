beb65a769e6c003f5f43eefa1646476bb40d1c95
igor.guariroba <igorguariroba@movida.com.br>
2022-09-21 21:11:06 -0300

    Estrutura do post montada

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-import { Post } from "./Post"
 import { Header } from "./components/Header"
 import { Sidebar } from "./components/Sidebar";
+import { Post } from "./components/Post";
 
 
 import styles from './App.module.css';
@@ -10,9 +10,8 @@ export function App() {
   return (
     <div>
       <Header />
-
       <div className={styles.wrapper}>
-          <Sidebar/>
+        <Sidebar />
         <main>
           <Post
             author="Igor Guariroba"
---
+++ src/components/Post.jsx
@@ -0,0 +1,24 @@
+import styles  from "./Post.module.css";
+
+export function Post(props) {
+  return (
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}>
+          <img className={styles.avatar} src="https://avatars.githubusercontent.com/u/26732044?v=4" />
+        </div>
+        <div className={styles.authorInfo}>
+          <strong>Tobias Fried</strong>
+          <span>rektdeckard</span>
+        </div>
+
+        <time title='28 de Setembro as 08:48' dateTime="2022-08-28 08:48:00">Publicado há 1h</time>
+      </header>
+
+      <div className={styles.content}>
+        <p>{props.content}</p>
+      </div>
+
+    </article>
+  )
+}
