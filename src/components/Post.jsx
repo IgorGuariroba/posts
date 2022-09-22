2bd3afe9c2ce65823b266bd148974367f2c94a80
igor.guariroba <igorguariroba@movida.com.br>
2022-09-21 22:42:33 -0300

    Post header finalizado

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,4 +1,4 @@
-import styles  from "./Post.module.css";
+import styles from "./Post.module.css";
 
 export function Post(props) {
   return (
@@ -6,17 +6,24 @@ export function Post(props) {
       <header>
         <div className={styles.author}>
           <img className={styles.avatar} src="https://avatars.githubusercontent.com/u/26732044?v=4" />
+          <div className={styles.authorInfo}>
+            <strong>Tobias Fried</strong>
+            <span>rektdeckard</span>
+          </div>
         </div>
-        <div className={styles.authorInfo}>
-          <strong>Tobias Fried</strong>
-          <span>rektdeckard</span>
-        </div>
+
 
         <time title='28 de Setembro as 08:48' dateTime="2022-08-28 08:48:00">Publicado há 1h</time>
       </header>
 
       <div className={styles.content}>
         <p>{props.content}</p>
+        <p><a href="#">nova.desing/feitopormim</a></p>
+        <p>
+          <a href="#">#nova</a>{' '}
+          <a href="#">#desing</a>{' '}
+          <a href="#">#feitopormim</a>
+        </p>
       </div>
 
     </article>
